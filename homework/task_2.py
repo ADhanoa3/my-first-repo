7c0091013a93d56e6875bf3ea8602df8f09f971d
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 09:19:36 -0700

    Create task_2.py

---
+++ homework/task_2.py
@@ -0,0 +1,35 @@
+# Task: Combine computations with lists and print the results
+
+# 1. Define a variable called numbers as a nonempty list of numbers of your choosing.
+
+
+
+# 2. Use a built-in function to calculate the sum of the list numbers and set it to the variable sum_numbers.
+
+
+
+# 3. Print to the console Sum of all numbers: # where # is the value of sum_numbers.
+
+
+
+# 4. Use the variable sum_numbers and another built in function to calculate the average of the list numbers.
+# Set the average to the variable average_numbers.
+
+
+
+# 5. Print to console Average of numbers: # where # is the value of average_numbers.
+
+
+
+# 6. Use built-in functions to find the maximum and minimum numbers in the list numbers.
+# Set the values equal to max_number and min_number, resepctively.
+
+
+
+# 7. Print to console Maximum number: # where # is the value of max_number.
+
+
+
+# 8. Print to console Minimum number: # where # is the value of min_number.
+
+
