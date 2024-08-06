77fd43db7c86df9b68eb1222412a5c184c903dfa
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 09:31:50 -0700

    Create task_3.py

---
+++ homework/task_3.py
@@ -0,0 +1,18 @@
+# Task: Correct all the errors.
+
+# 1. Find and fix the error.
+print(Hello, world!)
+
+# 2. Find and fix the error.
+message = "Greetings'
+
+# 3. Find and fix the error.
+x = 5
+print("The value of x is" x)
+
+# 4. Find and fix the error.
+numbers = [1, 2, 3]
+print("The first number in the list numbers is", numbers[1])
+
+# 5. Find and fix the error.
+print("There are", length(numbers), "numbers in the list called numbers.")
