7c365fba99fc23dfd3f744d8c0046b332447207a
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 11:00:01 -0700

    Update test_task_3.py

--- homework/tests/test_task_3.py
+++ homework/tests/test_task_3.py
@@ -1,1 +1,37 @@
+import subprocess
+import os
+import sys
+
+# Add the parent directory to sys.path to enable import of exponentiation_task
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import task_3
+
+
+def test_console_output():
+    # Run the task script
+    result = subprocess.run(
+        ["python", "./homework/task_3.py"], capture_output=True, text=True
+    )
+
+    # Check the printed output
+    expected_output = (
+        f"Hello, world!\n"
+        f"The value of x is 5\n"
+        f"The first number in the list numbers is 1\n"
+        f"There are 3 numbers in the list called numbers.\n"
+    )
+    assert (
+        result.stdout == expected_output
+    ), f"Expected '{expected_output}' but got '{result.stdout}'"
+
+
+def test_message_variable():
+    message = task_3.message
+    assert message == "Greetings", f"Expected Greetings but got {message}"
+
+
+if __name__ == "__main__":
+    test_console_output()
+    test_message_variable()
 
