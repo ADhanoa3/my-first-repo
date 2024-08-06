a78f27bb78b3c38f5c838a25c7ff37d17e84aac7
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 09:13:52 -0700

    Update task_1.py

--- homework/task_1.py
+++ homework/task_1.py
@@ -1,7 +1,14 @@
-# Complete this task by adding lines of code underneath this comment block.
-# For your first task, define two variables called a and b with numerical values of your choosing.
-# Then define a third variable called result which is the result of raising a to the bth power.
+# Task: Complete each step below to print an exponentiation result to the console.
+
+# 1. Define two variables, called a and b, with numerical values of your choosing.
+
+
+
+# 2. Define a third variable called result which is the result of raising a to the power of b.
 # The variable result should be defined in terms of the variables a and b.
+
+
+
 # Print to the console Result: # where # is the value of the result variable.
 # For example, if a and b are 2 and 3 respectively, then the variable result should be 8
 # and Result: 8 should be printed to the console.
