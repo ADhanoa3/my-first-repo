fc53ad7f298758d0804f9ea19626584b2dea8df1
ADhanoa3 <adhanoa3@csustan.edu>
2024-08-27 18:55:57 -0700

    Update task_3.py

--- homework/task_3.py
+++ homework/task_3.py
@@ -1,18 +1,19 @@
 # Task: Correct all the errors.
 
 # 1. Find and fix the error.
-print(Hello, world!)
+print("Hello, world!")
 
 # 2. Find and fix the error.
-message = "Greetings'
+message = "Greetings"
+print(message)
 
 # 3. Find and fix the error.
 x = 5
-print("The value of x is" x)
+print("The value of x is", x)
 
 # 4. Find and fix the error.
 numbers = [1, 2, 3]
-print("The first number in the list numbers is", numbers[1])
+print("The first number in the list numbers is", numbers[0])
 
 # 5. Find and fix the error.
-print("There are", length(numbers), "numbers in the list called numbers.")
+print("There are", len(numbers), "numbers in the list called numbers.")
